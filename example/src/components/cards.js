167e81ece6d33c2dd63fd71434ee4fee1c33b178
Pyrax <piranhadev@gmail.com>
2020-06-06 17:58:20 +0200

    feat(docs): add examples index page

---
+++ example/src/components/cards.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+const gridStyles = (columns = 2) => {
+  return {
+    listStyle: 'none',
+    display: 'grid',
+    gridTemplateColumns: ['auto', null, `repeat(${columns}, 1fr)`],
+    gridGap: 4,
+    p: 0
+  }
+}
+
+export default ({ children, columns, ...props }) => (
+  <div
+    {...props}
+    sx={{
+      ol: gridStyles(columns),
+      ul: gridStyles(columns),
+      p: {
+        m: 0
+      },
+      a: {
+        variant: 'text.heading',
+        display: 'block',
+        fontWeight: 'bold',
+        fontSize: 3,
+        color: 'inherit',
+        textDecoration: 'none',
+        ':hover,:focus': {
+          color: 'primary'
+        }
+      }
+    }}
+  >
+    {children}
+  </div>
+)

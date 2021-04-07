22d4b246aac152c4432889a87839abc757ecd6d9
Pyrax <piranhadev@gmail.com>
2021-04-07 20:16:18 +0200

    style: fix line-ending and spaces

--- src/recaptcha.js
+++ src/recaptcha.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { useNetlifyFormContext } from './netlify-form-context'
 import GoogleRecaptcha from 'react-google-recaptcha'
 
-export const Recaptcha = ({ siteKey = '', invisible = false, ...passThroughProps }) => {
+export const Recaptcha = ({
+  siteKey = '',
+  invisible = false,
+  ...passThroughProps
+}) => {
   const { recaptchaRef } = useNetlifyFormContext()
 
   return (
-    <GoogleRecaptcha 
-      {...passThroughProps} 
-      sitekey={siteKey} 
-      ref={recaptchaRef} 
-      {...(invisible ? {size: 'invisible'} : {})} 
+    <GoogleRecaptcha
+      {...passThroughProps}
+      sitekey={siteKey}
+      ref={recaptchaRef}
+      {...(invisible ? { size: 'invisible' } : {})}
     />
   )
 }

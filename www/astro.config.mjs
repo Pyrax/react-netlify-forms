e5766dac4e39ab4eca8eab11467dbf42a35c20b6
Bjoern Clees <piranhadev@gmail.com>
2025-06-09 00:35:55 +0200

    chore(docs): migrate gatsby to astro.js

---
+++ www/astro.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'React Netlify Forms',
+			description: 'React components and hooks giving you the power of Netlify Forms',
+			logo: {
+				src: './src/assets/logo.png',
+				alt: 'react-netlify-forms',
+			},
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/pyrax/react-netlify-forms',
+				},
+			],
+			sidebar: [
+				{
+					label: 'Examples',
+					autogenerate: { directory: 'examples' },
+				},
+				{ slug: 'reference' },
+			],
+		}),
+		mdx(),
+	],
+	site: 'https://pyrax.github.io/react-netlify-forms',
+	base: '/react-netlify-forms',
+});
